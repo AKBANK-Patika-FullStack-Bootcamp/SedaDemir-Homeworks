bac37e5b8f743824a1665a08fc751bb7d64ab8e8
sed <sedademirgf@gmail.com>
2022-01-26 19:53:14 +0000

    part-2 v2

--- Homework-6/part-2/app.js
+++ Homework-6/part-2/app.js
@@ -1,15 +1,17 @@
-function girlsPower(arr) {
+const girlsPower = (arr) => {
   if (arr.every((n) => typeof n == "number")) {
-    const array = arr.map((num) => girlsPowerAddition(num));
-
-    function girlsPowerAddition(num) {
-      return num / 2 + 3;
-    }
+    const array = arr.map((num) => {
+      return girlsPowerAddition(num);
+    });
     return `[${arr.join(", ")}] => [${array.join(", ")}]`;
   } else {
     return `Each element must be a number.`;
   }
-}
+};
+
+const girlsPowerAddition = (num) => {
+  return num / 2 + 3;
+};
 
 const array1 = [2, 5, 4];
 const array2 = [10, 7, 21];
